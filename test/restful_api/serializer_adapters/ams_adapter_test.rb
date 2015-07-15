7bb81486e9c2894cd628770e9b0ca478ada97294
briandavidwetzel <briandavidwetzel@gmail.com>
2015-07-14 23:15:17 -0400

    serialize collections

--- test/restful_api/serializer_adapters/ams_adapter_test.rb
+++ test/restful_api/serializer_adapters/ams_adapter_test.rb
@@ -12,6 +12,20 @@ module RestfulApi
         assert_equal BrickSerializer, @adapter.klass
       end
 
+      def test_serialize
+        brick = Brick.create color: 'yellow', weight: 10, material: 'gold'
+        serialized_brick = @adapter.serialize brick
+        assert_equal "{\"brick\":{\"color\":\"yellow\",\"weight\":\"10.0\",\"material\":\"gold\"}}", serialized_brick
+      end
+
+      def test_serialize_collection
+        DatabaseCleaner.clean
+        brick1 = Brick.create color: 'yellow', weight: 10, material: 'gold'
+        brick2 = Brick.create color: 'red',    weight: 1,  material: 'clay'
+        serialized_brick_array = @adapter.serialize_collection Brick.all
+        assert_equal "[{\"color\":\"yellow\",\"weight\":\"10.0\",\"material\":\"gold\"},{\"color\":\"red\",\"weight\":\"1.0\",\"material\":\"clay\"}]", serialized_brick_array
+      end
+
     end
   end
 end
